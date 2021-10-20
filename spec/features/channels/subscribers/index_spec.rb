1feafa202dd74e0a1e8ac2fdc613414beedd8a14
Jacob Yarborough <jayarborough95@gmail.com>
2021-10-20 15:30:02 -0600

    Add user story 21

--- spec/features/channels/subscribers/index_spec.rb
+++ spec/features/channels/subscribers/index_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Channels subscribers index' do 
     before :each do
         @channel_1 = Channel.create!(name: 'ABC', cost: 25, spanish: false)
-        @sub_1 = @channel_1.subscribers.create!(name: 'Jacob', age: 25, top_market: false)
-        @sub_2 = @channel_1.subscribers.create!(name: 'John', age: 20, top_market: true)
+        @sub_1 = @channel_1.subscribers.create!(name: 'Jacob', age: 22, top_market: false)
+        @sub_2 = @channel_1.subscribers.create!(name: 'John', age: 35, top_market: true)
+        @sub_3 = @channel_1.subscribers.create!(name: 'Joey', age: 26, top_market: true)
     end 
     it 'shows all the subscribers of a channel' do
         visit "channels/#{@channel_1.id}/subscribers"
@@ -66,5 +67,27 @@ RSpec.describe 'Channels subscribers index' do
         expect(page).to have_content("Jacoby")
         expect(page).to have_content("27")
         expect(page).to have_content("true")
+    end 
+
+    it 'has a number entry form to limit number of results shown' do 
+        # As a visitor
+        # When I visit the Parent's children Index Page
+        # I see a form that allows me to input a number value
+        # When I input a number value and click the submit button that reads 'Only return records with more than `number` of `column_name`'
+        # Then I am brought back to the current index page with only the records that meet that threshold shown.
+
+        visit "/channels/#{@channel_1.id}/subscribers"
+
+        expect(current_path).to eq("/channels/#{@channel_1.id}/subscribers")
+
+        fill_in "Minimum age of subscribers", with: 25
+
+        click_button "Return Subscribers Based on Age"
+
+        expect(current_path).to eq("/channels/#{@channel_1.id}/subscribers")
+
+        expect(page).to have_content(@sub_2.name)
+        expect(page).to have_content(@sub_3.name)
+        expect(page).not_to have_content(@sub_1.name)
     end 
 end 
--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -5,23 +5,31 @@ RSpec.describe Channel do
 
   describe 'instance methods' do 
     describe '#sub_count' do 
-      before :each do 
-        @channel_1 = Channel.create(name: 'ABC', cost: 25, spanish: false)
-        @sub_1 = @channel_1.subscribers.create(name: 'Jacob', age: 25, top_market: false)
-      end 
       it 'returns the number of subscribers for a channel' do 
-        expect(@channel_1.sub_count).to eq(1)
+        channel_1 = Channel.create(name: 'ABC', cost: 25, spanish: false)
+        sub_1 = channel_1.subscribers.create(name: 'Jacob', age: 25, top_market: false)
+
+        expect(channel_1.sub_count).to eq(1)
       end 
     end 
 
     describe '#recently_created' do 
-      before :each do 
-        @channel_1 = Channel.create(name: 'ABC', cost: 25, spanish: false)
-        @channel_2 = Channel.create(name: 'CBS', cost: 20, spanish: true)
-        @channel_3 = Channel.create(name: 'NBC', cost: 15, spanish: true)
-      end 
       it 'should return an array of channels with the most recently created channels coming first' do
-        expect(Channel.recently_created).to eq([@channel_3, @channel_2, @channel_1])
+        channel_1 = Channel.create(name: 'ABC', cost: 25, spanish: false)
+        channel_2 = Channel.create(name: 'CBS', cost: 20, spanish: true)
+        channel_3 = Channel.create(name: 'NBC', cost: 15, spanish: true)
+
+        expect(Channel.recently_created).to eq([channel_3, channel_2, channel_1])
+      end 
+    end
+    
+    describe '#index_view' do 
+      it 'can restrict the view of the index if there is a criteria entered' do 
+        channel_1 = Channel.create(name: 'ABC', cost: 25, spanish: false)
+        sub_1 = subscribers.create(name: 'Jacob', age: 25, top_market: false, channel_id: 799)
+        sub_2 = subscribers.create(name: 'Joey', age: 28, top_market: false, channel_id: 799)
+        params = {"utf8"=>"✓", "minimum_age_of_subscribers"=>"25", "commit"=>"Return Subscribers Based on Age", "controller"=>"channel_subscribers", "action"=>"index", "channel_id"=>"799"}
+        expect(channel_1.index_view(params).length).to eq(1)
       end 
     end 
   end 
